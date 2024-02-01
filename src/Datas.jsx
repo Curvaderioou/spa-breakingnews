940f6cc88e9b13b1f4808e64a3984cd82c70ab79
devDigitalMix <digitalmixdev@gmail.com>
2024-02-01 09:22:08 -0300

    navbar

---
+++ src/Datas.jsx
@@ -0,0 +1,34 @@
+export const news = [
+  {
+    title: "Notícia 1",
+    text: "lorem ipsum dolor sit amet con con et con et",
+    image:
+      "https://imgb.ifunny.co/images/951d3730ac2e43f9426085daa63c01573bb0344fc3ddeeecf7e6239457c169a0_1.jpg",
+    likes: 2,
+    comments: 4,
+  },
+  {
+    title: "Notícia 2",
+    text: "lorem ipsum dolor sit amet con con et con et",
+    image:
+      "https://imgb.ifunny.co/images/951d3730ac2e43f9426085daa63c01573bb0344fc3ddeeecf7e6239457c169a0_1.jpg",
+    likes: 5,
+    comments: 2,
+  },
+  {
+    title: "Notícia 3",
+    text: "lorem ipsum dolor sit amet con con et con et",
+    image:
+      "https://imgb.ifunny.co/images/951d3730ac2e43f9426085daa63c01573bb0344fc3ddeeecf7e6239457c169a0_1.jpg",
+    likes: 2,
+    comments: 2,
+  },
+  {
+    title: "Notícia 4",
+    text: "lorem ipsum dolor sit amet con con et con et",
+    image:
+      "https://imgb.ifunny.co/images/951d3730ac2e43f9426085daa63c01573bb0344fc3ddeeecf7e6239457c169a0_1.jpg",
+    likes: 10,
+    comments: 5,
+  },
+];
