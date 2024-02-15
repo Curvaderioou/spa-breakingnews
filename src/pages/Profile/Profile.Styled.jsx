8bc222a5133e492efd41fe72c303d99f7db27a35
devDigitalMix <digitalmixdev@gmail.com>
2024-02-15 14:23:01 -0300

    perfil

--- src/pages/Profile/Profile.Styled.jsx
+++ src/pages/Profile/Profile.Styled.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const ProfileContainer = styled.section`
   max-width: 1200px;
+  width: 100%;
   margin: 0 auto;
   display: flex;
   flex-direction: column;
